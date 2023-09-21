79e3a9ec3bf587f7135b31c02ef8704cdcc3f7d5
Vic-toru <140169444+Vic-toru@users.noreply.github.com>
2023-09-21 16:54:36 +0200

    ajout exo algoblocs

---
+++ 5_Exercices_En_Cours_Formation/Exercices Algo/algoblocs.js
@@ -0,0 +1,892 @@
+// Niveau A1 Algoblocs
+// Initialisation
+/*
+setPos(300,300);
+faceDown();
+setLineWidth(10);
+changeColor(color.red);
+
+//Réalisation
+
+forward(200);
+faceRight();
+forward(100);
+*/
+
+/*
+// Niveau A2 Algoblocs
+//Initialisation
+setPos(300,300);
+faceLeft();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+forward(50);
+faceDown();
+forward(50);
+faceRight();
+forward(100);
+faceUp();
+forward(100);
+*/
+
+/*
+//Niveau A3 Algoblocs
+//Initialisation
+setPos(300,300);
+faceLeft();
+setLineWidth(10);
+changeColor(color.yellow)
+
+//Réalisation
+forward(50);
+faceRight();
+forward(100);
+faceLeft();
+forward(50);
+faceUp();
+forward(50);
+faceDown();
+forward(100);
+*/
+
+/*
+//Niveau A4 Algoblocs
+//Initialisation
+setPos(200,400);
+faceRight();
+setLineWidth(10);
+changeColor(color.green);
+
+//Realisation
+forward(100);
+arcLeft(50,180);
+forward(50);
+arcRight(50,180);
+forward(100);
+*/
+
+/*
+//Niveau A5 Algoblocs
+//Initialisation
+setPos(300,200);
+faceRight();
+setLineWidth(10);
+changeColor(color.fuchsia)
+
+//Realisation
+arcRight(100,360);
+arcRight(50,180);
+arcLeft(50,180);
+*/
+
+/*
+//Niveau A6 Algoblocs
+//Initialisation
+setPos(300,300);
+faceUp();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+*/
+
+/*
+//Niveau A7 Algoblocs
+//Initialisation
+setPos(200,300);
+faceDown();
+setLineWidth(10);
+changeColor(color.green);
+
+//Realisation
+forward(100);
+arcLeft(100,180);
+forward(50);
+up();
+forward(50);
+down();
+forward(100);
+arcLeft(100,180);
+forward(50);
+*/
+
+/*
+//Niveau A8 Algoblocs
+//Initialisation
+setPos(300,300);
+faceUp();
+setLineWidth(10);
+changeColor(color.fuchsia);
+
+//Realisation
+forward(50);
+faceRight();
+forward(100);
+faceDown();
+forward(200);
+faceLeft();
+forward(50);
+up();
+forward(50);
+faceUp();
+forward(50);
+down();
+forward(50);
+*/
+
+/*
+//Niveau A9 Algoblocs
+//Initialisation
+setPos(200,400);
+faceRight();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+forward(200);
+faceLeft();
+arcRight(50,180);
+arcLeft(50,180);
+forward(200);
+arcLeft(50,180);
+arcRight(50,180);
+*/
+
+/*
+//Niveau A10 Algoblocs
+//Initialisation
+setPos(300,300);
+faceRight();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+arcLeft(50,180);
+changeColor(color.green);
+arcLeft(100,180);
+changeColor(color.yellow);
+arcLeft(200,180)
+*/
+
+/*
+//Niveau A11 Algoblocs
+//Initialisation
+setPos(75,200);
+faceUp();
+setLineWidth(10);
+changeColor(color.blue);
+
+//Realisation
+arcRight(100,180);
+faceRight();
+up();
+forward(50);
+down();
+faceUp();
+arcRight(100,180);
+up();
+faceLeft();
+forward(25);
+faceDown();
+forward(25);
+changeColor(color.fuchsia);
+down();
+arcRight(200,180);
+*/
+
+/*
+//Niveau A12 Algoblocs
+//Initialisation
+setPos(50,200);
+faceUp();
+setLineWidth(10);
+changeColor(color.green);
+
+//Realisation
+arcRight(50,180);
+forward(100);
+arcLeft(50,180);
+up();
+faceRight();
+forward(100);
+changeColor(color.yellow);
+down();
+faceDown();
+arcLeft(50,180);
+forward(100);
+arcRight(50,180);
+*/
+
+/*
+//Niveau B1 Algoblocs
+//Initialisation
+setPos(150,150);
+faceRight();
+setLineWidth(10);
+changeColor(color.green);
+
+//Realisation
+for(let i = 0 ; i < 5 ; i++) {
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+}
+*/
+
+/*
+//Niveau B2 Algoblocs
+//Initialisation
+setPos(50,300);
+faceUp();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++) {
+    forward(50);
+    faceRight();
+    forward(50);
+    faceDown();
+    forward(50);
+    faceRight();
+    forward(50);
+    faceUp();
+}
+*/
+
+/*
+//Niveau B3 Algoblocs
+//Initialisation
+setPos(200,200);
+faceRight();
+setLineWidth(10);
+changeColor(color.fuchsia);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++ ) {
+    forward(100);
+    right(90);
+}
+*/
+
+/*
+//Niveau B4 Algoblocs
+//Initialisation
+setPos(250,350);
+faceRight();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+for(let i = 0 ; i < 3 ; i++ ){
+    forward(100);
+    left(120);
+}
+*/
+
+/*
+//Niveau B5 Algoblocs
+//Initialisation
+setPos(300,300);
+faceUp();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realsiation
+forward(100);
+changeColor(color.red);
+for(let i =0 ; i < 3 ; i++ ) {
+    forward(50);
+    right(120);
+}
+*/
+
+/*
+//Niveau B6 Algoblocs
+//Initialisation
+setPos(150,350);
+faceRight();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+for(let i = 0 ; i < 2 ; i++ ){
+    forward(50);
+    left(90);
+    forward(50);
+    faceRight();
+}
+changeColor(color.green);
+for(let i = 0 ; i < 3 ; i++ ){
+    forward(50);
+    right(90);
+    forward(50);
+    faceRight();
+}
+*/
+
+/*
+//Niveau B7 Algoblocs
+//Initialisation
+setPos(300,300);
+faceRight();
+setLineWidth(10);
+changeColor(color.yellow)
+
+//Realisation
+for(let i = 0 ; i < 3 ; i++ ) {
+    forward(100);
+    left(120);
+}
+for(let i = 0 ; i < 4 ; i++ ) {
+    forward(100);
+    right(90);
+}
+*/
+
+/*
+//Niveau B8 Algoblocs
+//Initialisation
+setPos(300,300);
+faceRight();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+for(let i = 0 ; i < 8 ; i++ ) {
+    forward(200);
+    right(135);
+}
+*/
+
+/*
+//Niveau B9 Algoblocs
+//Initialisation
+setPos(150,300)
+faceRight();
+setLineWidth(10);
+changeColor(color.fuchsia);
+
+//Realisation
+forward(50);
+up();
+forward(50);
+down();
+for(let i = 0 ; i < 3 ; i++) {
+    forward(100);
+    right(120);
+}
+*/
+
+/*
+//Niveau B10 Algoblocs
+//Initialisation
+setPos(50,300)
+faceRight();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+for(let i = 0 ; i < 9 ; i++ ){
+    forward(100);
+    right(135);
+}
+
+up();
+left(135);
+forward(200);
+down();
+changeColor(color.yellow);
+
+for(let i = 0 ; i < 8 ; i++ ){
+    forward(200);
+    right(135);
+}
+
+up();
+right(120);
+forward(200);
+changeColor(color.fuchsia);
+down();
+
+for(let i = 0 ; i < 8 ; i++){
+    forward(100);
+    left(135);
+}
+*/
+
+/*
+//Niveau B11 Algoblocs
+//Initialisation
+setPos(250,250);
+faceRight();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++ ){
+    forward(50);
+    left(90);
+    forward(50);
+    right(90);
+    forward(50);
+    right(90);
+}
+*/
+
+/*
+//Niveau B12 Algoblocs
+//Initialisation
+setPos(350,350);
+faceRight();
+setLineWidth(10);
+changeColor(color.fuchsia);
+
+//Realisation
+faceDown();
+for(let i = 0 ; i < 3 ; i++) {
+    forward(50);
+    right(90);
+}
+faceUp()
+up();
+forward(50);
+changeColor(color.green);
+down();
+
+for(let i = 0 ; i < 3 ; i++ ){
+    forward(50);
+    right(90);
+}
+*/
+
+/*
+//Niveau B13 Algoblocs
+//Initialisation
+setPos(250,250);
+faceRight();
+setLineWidth(10);
+changeColor(color.green);
+
+//Realisation
+for(let i = 0 ; i < 8 ; i++ ) {
+    forward(50);
+    left(135);
+    forward(25);
+    right(90);
+    forward(25);
+    right(90);
+}
+*/
+
+/*
+//Niveau B14 Algoblocs
+//Initialisation
+setPos(100,100);
+faceRight();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+forward(50);
+for(let i = 0 ; i < 8 ; i++){
+    right(120);
+    forward(100);
+    left(120);
+    forward(100);
+}
+*/
+
+/*
+//Niveau B15 Algoblocs
+//Initialisation
+setPos(300,300);
+faceRight();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++ ){
+    forward(100);
+    arcLeft(50,90);
+}
+*/
+
+/*
+//Niveau B16 Algoblocs
+//Initialisation
+setPos(150,450);
+faceRight();
+setLineWidth(10);
+changeColor(color.green);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++){
+    arcLeft(25,90);
+    arcRight(25,90)
+}
+faceDown();
+arcRight(200,90);
+*/
+
+/*
+//Niveau B17 Algoblocs
+//Initialisation
+setPos(200,400);
+faceRight();
+setLineWidth(10);
+changeColor(color.blue);
+
+//Realisation
+for(let i = 0 ; i < 6 ; i++) {
+    arcLeft(100,180);
+    left(120);
+}
+*/
+
+/*
+//Niveau B18 Algoblocs
+//Initialisation
+setPos(250,350);
+faceRight();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+for(let i = 0 ; i < 8 ; i++ ) {
+    forward(200);
+    left(90);
+    forward(50);
+    left(45);
+}
+*/
+
+/*
+//Niveau B19 Algoblocs
+//Initialisation
+setPos(300,550);
+faceRight();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++ ) {
+    arcLeft(50,180)
+    faceRight();
+}
+up();
+faceUp();
+forward(25);
+down();
+
+for(let i = 0 ; i < 4 ; i++ ){
+    faceLeft();
+    arcLeft(50,180);
+}
+*/
+
+/*
+//Niveau B20 Algoblocs
+//Initialisation
+setPos(200,250);
+faceDown();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+for(let i = 0 ; i < 2 ; i++) {
+    forward(100);
+    arcLeft(50,90);
+}
+forward(50);
+for(let i = 0 ; i < 2 ; i++ ) {
+    forward(100);
+    arcLeft(50,90);
+}
+forward(50);
+faceRight();
+forward(200);
+backward(100);
+up();
+faceDown();
+forward(100);
+down();
+faceRight();
+arcLeft(25,360);
+*/
+
+/*
+//Niveau B21 Algoblocs
+//Initialisation
+setPos(400,100);
+faceRight();
+setLineWidth(10);
+shiftColor(0.5);
+
+//Realisation
+for(let i = 0 ; i < 6 ; i++ ){
+    forward(100);
+    for(let i = 0 ; i < 2 ; i++){
+        left(120);
+        forward(50);
+    }
+    forward(50);
+    right(60);
+    forward(50);
+    right(120);
+    forward(50);
+    right(120);
+    up();
+    forward(200);
+    down();
+    shiftColor(0.15);
+}
+*/
+
+/*
+//Niveau B22 Algoblocs
+//Initialisation
+setPos(300,350);
+faceRight();
+left(30);
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+for(let i = 0 ; i < 6 ; i++) {
+    forward(100);
+    left(60);
+}
+changeColor(color.red);
+left(210);
+for(let i = 0 ; i < 3 ; i++) {
+    forward(100);
+    left(120);
+}
+right(90);
+changeColor(color.green);
+for(let i = 0 ; i < 4 ; i++ ){
+    forward(100);
+    left(90);
+}
+right(210);
+for(let i = 0 ; i < 4 ; i++ ){
+    forward(100);
+    left(90);
+}
+*/
+
+/*
+//Niveau B23 Algoblocs
+//Initialisation
+setPos(150,300);
+faceRight();
+left(45);
+setLineWidth(10);
+changeColor(color.fuchsia);
+
+//Realisation
+for(let i = 0 ; i < 3 ; i++ ) {
+    forward(50);
+    right(90);
+    forward(50);
+    left(90);
+}
+right(135);
+forward(25);
+right(45);
+for(let i = 0 ; i < 3 ; i++ ) {
+    forward(50);
+    right(90);
+    forward(50);
+    left(90);
+}
+right(135);
+forward(25);
+*/
+
+/*
+//Niveau B24 Algoblocs
+//Initialisation
+setPos(250,150);
+faceRight();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+for(let i = 0 ; i < 8 ; i++ ) {
+    forward(100);
+    left(135);
+}
+forward(50);
+changeColor(color.green);
+for(let i = 0 ; i < 3 ; i++ ) {
+    right(135);
+    forward(100);
+    left(135);
+    forward(50)
+}
+forward(50);
+right(90);
+for(let i = 0 ; i < 3 ; i++) {
+    forward(25);
+    left(90);
+}
+faceRight();
+forward(50);
+for(let i = 0 ; i < 3 ; i++ ){
+    forward(50);
+    left(135);
+    forward(100);
+    right(135);
+}
+*/
+
+/*
+//Niveau C1 Algoblocs
+//Initialisation
+setPos(50,250);
+faceRight();
+setLineWidth(10);
+changeColor(color.aqua);
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++ ){
+    for(let i = 0 ; i < 3 ; i++ ){
+        forward(100);
+        right(120);
+    }
+    forward(100);
+}
+*/
+
+/*
+//Niveau C2 Algoblocs
+//Initialisation
+setPos(275,550);
+faceUp();
+setLineWidth(10);
+changeColor(color.fuchsia);
+
+//Realisation
+for(let i = 0 ; i < 5 ; i++ ){
+    for(let i = 0 ; i < 4 ; i++ ){
+        forward(50);
+        right(90);
+    }
+    up();
+    forward(100);
+    down();
+}
+*/
+
+/*
+//Niveau C3 Algoblocs
+//Initialisation
+setPos(50,325);
+faceRight();
+setLineWidth(10);
+shiftColor(0.4);
+
+//Realisation
+for(let i = 0 ; i < 3 ; i++ ){
+    for(let i = 0 ; i < 8 ; i++ ){
+        forward(100);
+        left(135);
+    }
+    up();
+    forward(200);
+    shiftColor(0.1);
+    down();
+}
+*/
+
+/*
+//Niveau C4 Algoblocs
+//Initialisation
+setPos(50,300);
+faceRight();
+setLineWidth(10);
+changeColor(color.red);
+
+//Realisation
+for(let i = 0 ; i < 5 ; i++){
+    for(let i = 0 ; i < 3 ; i++){
+        forward(50);
+        left(120);
+    }
+    changeColor(color.yellow);
+    for(let i = 0 ; i < 4 ; i++){
+        forward(50);
+        right(90);
+    }
+    up();
+    forward(100);
+    changeColor(color.red)
+    down();
+}
+*/
+
+/*
+//Niveau C5 Algoblocs
+//Initialisation
+setPos(300,300);
+faceRight();
+setLineWidth(10);
+shiftColor(0.2)
+
+//Realisation
+for(let i = 0 ; i < 4 ; i++){
+    for(let i = 0 ; i < 8 ; i++){
+        forward(200);
+        left(135);
+    }
+    shiftColor(0.25);
+    right(90);
+}
+*/
+
+/*
+//Niveau C6 Algoblocs
+//Initialisation
+setPos(300,300);
+faceRight();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+for(let i = 0 ; i < 3 ; i++){
+    for(let i = 0 ; i < 5 ; i++){
+        forward(100);
+        left(120);
+    }
+    right(120)
+}
+*/
+
+/*
+//Niveau C7 Algoblocs
+//Initialisation
+setPos(300,400);
+faceRight();
+setLineWidth(10);
+changeColor(color.yellow);
+
+//Realisation
+for(let i = 0 ; i < 8 ; i++){
+    for(let i = 0 ; i < 3 ; i++){
+        forward(50);
+        right(120);
+    }
+    forward(50);
+    left(45);
+}
+*/
