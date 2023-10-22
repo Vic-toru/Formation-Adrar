963372bc53a2d86c4447e65e8111a6543dc1c115
Victor <rodrigues.vmm@gmail.com>
2023-10-22 14:48:57 +0200

    ajout projet nekotaku

---
+++ 5_Exercices_En_Cours_Formation/Exercices Algo/Exercice 5/divers.js
@@ -0,0 +1,27 @@
+// Exercice divers_1 :	écrire une fonction qui retourne **la position** du plus grand élément d'un tableau.
+// Exemple : positionBiggest([4,7,5,5,6]) retourne 1 car le plus grand est dans la case numéro 1
+/*let tab = [5,8,6,100,9];
+
+function positionBiggest(tab){
+    for let(i=0 ; i < tab.lenght ; i++){
+
+    }
+}
+console.log(positionBiggest([5,8,6,100,9]));*/
+
+// Exercice divers_2 : écrire une fonction qui fait la somme de tous les éléments d'un tableau.
+/*
+let tab1 = [0,1,2,3,4];
+
+function add(tab1){
+    let result = 0;
+    for (let i = 0; i < tab1.lenght ; i++){
+        result = result + tab1[i];
+    }
+}
+
+console.log(add(tab1));*/
+
+// Exercice divers_3 : écrire une fonction qui retourne la somme des valeurs positives d'un tableau.
+
+// Exercice divers_4 : En utilisant les deux fonctions précédentes, écrire une fonction qui retourne la somme des valeurs négatives d'un tableau.
